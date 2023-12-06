b8d1abc201fbc52209e2e9c2af1fdf14ab01bd46
N-Wallker <36032709+N-Wallker@users.noreply.github.com>
2023-12-06 12:50:57 +0100

    06-12-2023

---
+++ JS/Intro-JS.js
@@ -0,0 +1,27 @@
+// Data Types In JS
+// Strings
+// Number
+// Boolean
+// Arrays
+// Object
+// Symbol
+// null
+// undefined
+let z ="SCHOOL"
+console.log(`WELCOME BACK ${z}`);
+// var w = 10+2*4/2;
+// console.log(w);
+ let w = new Array (10);
+ console.log(typeof w);
+
+
+//  Types of Arrays
+let numeric = ["a",20,true,null,undefined,NaN]
+let object = {}
+let assoc = {
+    obirin: "Dupe",
+    okunrin: "Dele",
+    eranko: "Alangba",
+}
+console.log(assoc["eranko"]);
+console.log(assoc.okunrin);
---
+++ TEST#4/JS_TEST.js
@@ -0,0 +1,5 @@
+// JS CODE TO CALC CUBEROOT
+
+const cr = 27;
+const cubeRoot = Math.cbrt(cr);
+console.log(`the cube root is: ${cubeRoot}`);
